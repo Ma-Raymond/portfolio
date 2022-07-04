b425bed9abe1885817e06038f41bbf1edab56346
Raymond Ma <raymond.ma03@outlook.com>
2022-07-04 10:54:07 -0400

    added link to linkedin

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,7 +5,7 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsLinkedin/>
+              <a href='https://www.linkedin.com/in/raymond-ma03/'  target='_blank' rel='noreferrer' ><BsLinkedin/></a>
         </div>
     </div>
   )
